7014cc89a76fb2466538911538aa4cea5fa1d202
aldanisvigo@gmail.com <Ilovejenn2>
2015-12-28 22:08:13 -0800

    Cleaned Up Code

--- SerialPortExample/Program.cs
+++ SerialPortExample/Program.cs
@@ -17,74 +17,8 @@ using System.Drawing.Drawing2D;
 using System.Runtime.InteropServices;
 using System.Reflection;
 
-namespace SerialPortExample
-{
-
-	class ServoDisplay : Form{
-		public Image knob{ get; set; }
-		private int dAngle;
-		private System.Windows.Forms.Label AngleValue;
-		private System.Windows.Forms.Timer updateTimer;
-		public int setAngle{
-			get{
-				return this.dAngle;
-			}
-			set{ 
-				this.dAngle = value;
-			}
-		}
-		public ServoDisplay(Image knobImg){
-			//Constructor
-			Text = "Servo Simulator";
-			updateTimer = new System.Windows.Forms.Timer();
-			updateTimer.Interval = 100;
-			updateTimer.Tick += new EventHandler (Redraw_Form);
-			updateTimer.Enabled = true;
-			knob = knobImg;
-			Width = knob.Width + 20;
-			Height = knob.Height + 40;
-			AngleValue = new Label ();
-			AngleValue.Text = dAngle.ToString();
-			AngleValue.Location = new Point (0, 0);
-			AngleValue.Font = new Font ("Impact", 15.8f);
-			AngleValue.Width = 60;
-			Controls.Add (AngleValue);
-			this.DoubleBuffered = true;
-		}
-		private void Redraw_Form(Object sender, EventArgs e){
-			this.Invalidate ();
-		}
-		protected override void OnPaint(PaintEventArgs e){
-			if (knob != null) {
-				
-				Bitmap newKnob = RotateImage ((Bitmap)knob,dAngle);
-				e.Graphics.DrawImage (newKnob,new Point(0,0));
-
-				AngleValue.Text = dAngle.ToString () + "°";
-				//Refresh ();
-			} 
-		}
-		private Bitmap RotateImage(Bitmap b, float angle)
-		{
-			//Create a new empty bitmap to hold rotated image.
-			Bitmap returnBitmap = new Bitmap(b.Width, b.Height);
-			//Make a graphics object from the empty bitmap.
-			Graphics g = Graphics.FromImage(returnBitmap);
-			//move rotation point to center of image.
-			g.InterpolationMode = InterpolationMode.HighQualityBicubic;
-			g.TranslateTransform((float) b.Width / 2, (float)b.Height / 2);
-			//Rotate.        
-			g.RotateTransform(angle);
-			//Move image back.
-			g.TranslateTransform(-(float)b.Width / 2, -(float)b.Height / 2);
-			//Draw passed in image onto graphics object.
-			g.DrawImage(b, new Point(0, 0));
-			return returnBitmap;
-		}
-
-	}
-	
-	public static class MainClass{
+namespace SerialPortExample{
+	class MainClass{
 		//Windows API call to hide Console Windows
 		[DllImport("user32.dll")]
 		public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
@@ -107,19 +41,16 @@ namespace SerialPortExample
 		static SerialPort myPort;
 		static char baudupdownflag;
 		static string incoming_data;
-		public static void Main (string[] args)
-		{     
+		public static void Main (string[] args){     
 			//Remove this code to show Console Window
 			IntPtr hConsole = GetConsoleWindow();
-			if (IntPtr.Zero != hConsole)
-			{
-				ShowWindow(hConsole, 0); 
+			if (IntPtr.Zero != hConsole){
+				ShowWindow(hConsole,0); 
 			}
-				
 			Image servoKnob = Bitmap.FromFile ("servohead.png");
 			MyServoDisplay = new ServoDisplay (servoKnob);
 			ports = System.IO.Ports.SerialPort.GetPortNames();
-			InitWindow ();
+			InitWindow();
 		}
 		public static void myPort_DataReceived(Object sender, SerialDataReceivedEventArgs e){
 			incoming_data = myPort.ReadLine();
@@ -130,27 +61,15 @@ namespace SerialPortExample
 				}));
 
 			}catch(Exception ex){
-				MessageBox.Show (ex.Message.ToString ());
+				MessageBox.Show(ex.Message.ToString());
 			}
-			//if(IsNumeric(incoming_data)){
-			//	//MyServoDisplay.setAngle = Convert.ToInt16(incoming_data);
-			//	MyServoDisplay.setAngle = int.Parse(incoming_data);
-			//}
 			int num;
-			bool isNum = int.TryParse (incoming_data, out num);
-			if (isNum) {
-				MyServoDisplay.setAngle = int.Parse (incoming_data);
+			bool isNum = int.TryParse (incoming_data,out num);
+			if(isNum){
+				MyServoDisplay.setAngle = int.Parse(incoming_data);
 			}
 			Console.WriteLine("Angle:" + incoming_data);
 
-		}
-		public static bool IsNumeric(this string s){
-			foreach (char c in s){
-				if (!char.IsDigit(c) && c != '.'){
-					return false;
-				}
-			}
-			return true;
 		}
 		public static void InitWindow(){
 			//Main Window
@@ -175,7 +94,6 @@ namespace SerialPortExample
 			myTimer.Enabled = false;
 			myTimer.Tick += new EventHandler(myTimerEvent);
 
-
 			//BaudPicker
 			//Decrease Baud
 			baudMinus = new System.Windows.Forms.Button();
@@ -195,49 +113,46 @@ namespace SerialPortExample
 			//Increase Baud
 			baudPlus = new System.Windows.Forms.Button();
 			baudPlus.Text = "+";
-			baudPlus.MouseDown += new MouseEventHandler (IncreaseBaud);
+			baudPlus.MouseDown += new MouseEventHandler(IncreaseBaud);
 			baudPlus.MouseUp += new MouseEventHandler (StopMyTimer);
-			baudPlus.Location = new System.Drawing.Point (txtBaud.Location.X + txtBaud.Width + 10,5);
+			baudPlus.Location = new System.Drawing.Point(txtBaud.Location.X + txtBaud.Width + 10,5);
 			baudPlus.Width = baudPlus.Width / 2;
 			baudPlus.Font = new Font ("Impact", 20f);
 
 
 			//Connect Button
-			ConnectButton = new System.Windows.Forms.Button ();
+			ConnectButton = new System.Windows.Forms.Button();
 			ConnectButton.Text = "Connect";
-			ConnectButton.Font = new Font ("Impact", 9f);
+			ConnectButton.Font = new Font("Impact", 9f);
 			ConnectButton.Location  = new System.Drawing.Point(baudPlus.Location.X + baudPlus.Width + 10,5);
-			ConnectButton.Click += new EventHandler (ConnectButton_Click);
+			ConnectButton.Click += new EventHandler(ConnectButton_Click);
 
 
 
 			//termDisplay Label
 			termDisplay = new System.Windows.Forms.ListBox();
-			termDisplay.Location = new System.Drawing.Point (PortList.Location.X, ConnectButton.Location.Y + 30);
+			termDisplay.Location = new System.Drawing.Point(PortList.Location.X, ConnectButton.Location.Y + 30);
 			termDisplay.ForeColor = Color.LightGreen;
 			termDisplay.BackColor = Color.Black;
 			termDisplay.Height = 400;
 			termDisplay.Items.Add((PortList.Text == String.Empty?"NOPORT":PortList.Text) + "$>You need to press Connect!");
 
-			//termDisplay.UseWaitCursor = true;
-
+			//Set Window and Terminal ListBox Size
 			MyWindow.Size = new System.Drawing.Size(PortList.Location.X + PortList.Width + 10 + baudMinus.Width + 10 + txtBaud.Width + 10 + baudPlus.Width + 10 + ConnectButton.Width + 25 + MyWindow.Width,termDisplay.Location.Y + termDisplay.Height + 49);
 			termDisplay.Width = MyWindow.Size.Width - 36;
+
 			//Combine Controls
-			MyWindow.Controls.Add (ConnectButton);
-			MyWindow.Controls.Add (PortList);
-			MyWindow.Controls.Add (baudMinus);
-			MyWindow.Controls.Add (baudPlus);
-			MyWindow.Controls.Add (txtBaud);
-			MyWindow.Controls.Add (termDisplay);
+			MyWindow.Controls.Add(ConnectButton);
+			MyWindow.Controls.Add(PortList);
+			MyWindow.Controls.Add(baudMinus);
+			MyWindow.Controls.Add(baudPlus);
+			MyWindow.Controls.Add(txtBaud);
+			MyWindow.Controls.Add(termDisplay);
+
 			//Show and Run
-		
-			//myBrush = (Brush)Brushes.Black;
-
-
-			MyServoDisplay.Show ();
-			MyWindow.Show ();
-			Application.Run ();
+			MyServoDisplay.Show();
+			MyWindow.Show();
+			Application.Run();
 		}
 		private static void DecreaseBaud(object sender,MouseEventArgs e){
 			baudupdownflag = 'd';	//Decrease flag set
@@ -248,50 +163,49 @@ namespace SerialPortExample
 		}
 		static void myTimerEvent (object sender,EventArgs e){
 			int baud = Convert.ToInt16 (txtBaud.Text);
-			if(baudupdownflag == 'u') {
+			if(baudupdownflag == 'u'){
 				baud++;
 			}
-			if (baudupdownflag == 'd') {
+			if (baudupdownflag == 'd'){
 				baud--;
 			}
-			txtBaud.Text = baud.ToString ();
+			txtBaud.Text = baud.ToString();
 		}
 		static void IncreaseBaud(object sender, EventArgs e){
 			baudupdownflag = 'u';	//Increase flag set
 			myTimer.Enabled = true; //Start the timer
 		}
 		static void MyWindow_Close(object sender, FormClosedEventArgs e){
-			MyServoDisplay.Dispose ();
-			MyWindow.Dispose ();
-			System.Environment.Exit (1);
+			MyServoDisplay.Dispose();
+			MyWindow.Dispose();
+			System.Environment.Exit(1);
 		}
 		//Connect Button Click Event Handler
 		static void ConnectButton_Click(object sender,EventArgs e){
-			if (ConnectButton.Text == "Connect") {
+			if(ConnectButton.Text == "Connect"){
 				MessageBox.Show ("Connecting to " + PortList.Text + " at " + txtBaud.Text);
 				myPort = new SerialPort (PortList.Text, Convert.ToInt16 (txtBaud.Text), Parity.None, 8, StopBits.One);	//Create a port handle
-				try {
-					if (!myPort.IsOpen) {
-						myPort.Open ();
-						if (myPort.IsOpen) {
-							MessageBox.Show ("Successfuly opened the " + PortList.Text + "!"); 
+				try{
+					if(!myPort.IsOpen){
+						myPort.Open();
+						if (myPort.IsOpen){
+							MessageBox.Show("Successfuly opened the " + PortList.Text + "!"); 
 							PortList.Enabled = false;
 							txtBaud.Enabled = false;
 							baudMinus.Enabled = false;
 							baudPlus.Enabled = false;
 							ConnectButton.Text = "Disconnect";
-							myPort.DataReceived += new SerialDataReceivedEventHandler (myPort_DataReceived); //Assign the received data handler
-
-						} else {
-							MessageBox.Show ("There was an error opening the " + PortList.Text + " port!");
+							myPort.DataReceived += new SerialDataReceivedEventHandler(myPort_DataReceived); //Assign the received data handler
+						}else {
+							MessageBox.Show("There was an error opening the " + PortList.Text + " port!");
 						}
-					} else {
-						MessageBox.Show ("The " + PortList.Text + " port is already open!");
+					}else{
+						MessageBox.Show("The " + PortList.Text + " port is already open!");
 					}
-				} catch (Exception ew) {
-					MessageBox.Show (ew.Message.ToString ());
+				}catch(Exception ew){
+					MessageBox.Show(ew.Message.ToString());
 				}
-			} else if (ConnectButton.Text == "Disconnect") {
+			}else if(ConnectButton.Text == "Disconnect"){
 				try{
 					if(myPort.IsOpen){
 						String portName = myPort.PortName.ToString();
@@ -305,13 +219,12 @@ namespace SerialPortExample
 							ConnectButton.Text = "Connect";
 							termDisplay.Items.Clear();
 							termDisplay.Items.Add((PortList.Text == String.Empty?"NOPORT":PortList.Text) + "$>You need to press Connect!");
-						}
-						else{
+						}else{
 							MessageBox.Show("There was a problem disconnecting from the " + portName + "port.Please try again.");
 						}
 					}
-				}catch (Exception ex){
-					MessageBox.Show (ex.Message.ToString ());
+				}catch(Exception ex){
+					MessageBox.Show(ex.Message.ToString());
 				}
 			}
 		}
---
+++ SerialPortExample/ServoDisplay.cs
@@ -0,0 +1,85 @@
+﻿/*
+ * Author: Aldanis Vigo
+ * Date: 12/28/2015
+ * 
+ * 
+ * 	Before you run this, make sure you place the servohead.png file inside the SerialPortExample/bin/Debug folder. You can also put whatever image you want and just change the name to 
+ *  servohead.png.
+*/
+
+using System;
+using System.IO.Ports;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Timers;
+using System.Threading;
+using System.Drawing.Drawing2D;
+using System.Runtime.InteropServices;
+using System.Reflection;
+
+namespace SerialPortExample
+{
+	class ServoDisplay : Form{
+		public Image knob{ get; set; }
+		private int dAngle;
+		private System.Windows.Forms.Label AngleValue;
+		private System.Windows.Forms.Timer updateTimer;
+		public int setAngle{
+			get{
+				return this.dAngle;
+			}
+			set{ 
+				this.dAngle = value;
+			}
+		}
+		public ServoDisplay(Image knobImg){
+			//Constructor
+			Text = "Servo Simulator";
+			updateTimer = new System.Windows.Forms.Timer();
+			updateTimer.Interval = 100;
+			updateTimer.Tick += new EventHandler (Redraw_Form);
+			updateTimer.Enabled = true;
+			knob = knobImg;
+			Width = knob.Width + 20;
+			Height = knob.Height + 40;
+			AngleValue = new Label ();
+			AngleValue.Text = dAngle.ToString();
+			AngleValue.Location = new Point (0, 0);
+			AngleValue.Font = new Font ("Impact", 15.8f);
+			AngleValue.Width = 60;
+			Controls.Add (AngleValue);
+			this.DoubleBuffered = true;
+		}
+		private void Redraw_Form(Object sender, EventArgs e){
+			this.Invalidate ();
+		}
+		protected override void OnPaint(PaintEventArgs e){
+			if (knob != null) {
+
+				Bitmap newKnob = RotateImage ((Bitmap)knob,dAngle);
+				e.Graphics.DrawImage (newKnob,new Point(0,0));
+
+				AngleValue.Text = dAngle.ToString () + "°";
+				//Refresh ();
+			} 
+		}
+		private Bitmap RotateImage(Bitmap b, float angle)
+		{
+			//Create a new empty bitmap to hold rotated image.
+			Bitmap returnBitmap = new Bitmap(b.Width, b.Height);
+			//Make a graphics object from the empty bitmap.
+			Graphics g = Graphics.FromImage(returnBitmap);
+			//move rotation point to center of image.
+			g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+			g.TranslateTransform((float) b.Width / 2, (float)b.Height / 2);
+			//Rotate.        
+			g.RotateTransform(angle);
+			//Move image back.
+			g.TranslateTransform(-(float)b.Width / 2, -(float)b.Height / 2);
+			//Draw passed in image onto graphics object.
+			g.DrawImage(b, new Point(0, 0));
+			return returnBitmap;
+		}
+	}
+}
+
